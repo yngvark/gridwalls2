209a9a8da5a5d3d31d0ef38c72dc41de16319fbc
Yngvar Kristiansen <yngvark@gmail.com>
2016-07-14 12:53:10 +0000

    Created working build with dependency on rabbitmq.

---
+++ source/rabbitmq-test/src/main/java/test/Send.java
@@ -0,0 +1,14 @@
+package test;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+public class Send {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv)
+            throws java.io.IOException {
+
+    }
+}
