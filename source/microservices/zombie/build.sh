70aade4278242dd490433c10968569d968e7dcd7
Yngvar Kristiansen <yngvark@gmail.com>
2017-05-25 08:49:56 +0000

    Updated build.sh to use gradle 3.5

--- source/microservices/zombie/build.sh
+++ source/microservices/zombie/build.sh
@@ -1,2 +1,2 @@
 #gradle build dist -p source 
-docker run --rm -v "$PWD":/project -w /project --name gradle gradle:3.4.1-jdk8 gradle build dist -p /project/source
+docker run --rm -v "$PWD":/project -w /project --name gradle gradle:3.5-jdk8 gradle build dist -p /project/source
