6758013bf8467ae9b719e0e7320001238cc9eb98
Yngvar Kristiansen <yngvark@gmail.com>
2017-05-28 14:11:15 +0200

    Added library for communication through named pipes.

---
+++ source/lib/communicate_through_named_pipes/src/main/java/com/yngvark/communicate_through_named_pipes/file_io/FileOpener.java
@@ -0,0 +1,25 @@
+package com.yngvark.communicate_through_named_pipes.file_io;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+public class FileOpener {
+    private final String fifoOutputFilename;
+
+    public FileOpener(String fifoOutputFilename) {
+        this.fifoOutputFilename = fifoOutputFilename;
+    }
+
+    public FileWriter openStream() throws FileNotFoundException {
+        System.out.println("Opening output file... " + fifoOutputFilename);
+        FileOutputStream fileOutputStream = new FileOutputStream(fifoOutputFilename);
+        System.out.println("Opening output file... done.");
+
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+
+        return new FileWriter(out);
+    }
+
+}
