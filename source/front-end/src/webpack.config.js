5061f6c50e9763cdb61adbd5be87514a6ffbb0df
Yngvar Kristiansen <yngkri@knowit.no>
2019-01-09 20:24:02 +0100

    Fix hot reload of source code

--- source/front-end/src/webpack.config.js
+++ source/front-end/src/webpack.config.js
@@ -1,3 +1,5 @@
+// https://stackoverflow.com/questions/36039146/webpack-dev-server-compiles-files-but-does-not-refresh-or-make-compiled-javascri
+
 module.exports = {
     mode: 'development',
     // context: __dirname + "/game",
@@ -12,9 +14,15 @@ module.exports = {
             }
         ]
     },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: __dirname + "/",
+        publicPath: "/js"
+    },
     output: {
-        // path: __dirname + "/dist",
+        path: __dirname + "/dist",
+        publicPath: "/js",
         filename: "bundle.js"
     },
-    watch: false
+    watch: false,
 }
